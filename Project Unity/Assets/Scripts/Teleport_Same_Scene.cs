25f296f036de3ce0c19f50e4a84cc5c4df4d493d
Xdarwin <guidrapbiz@hotmail.fr>
2015-06-23 15:59:39 +0200

    Le manoir est finis ainsi que les niveaux
    Pour entrer dans le manoir il faut appuyer sur E.
    C'est la même chose pour aller dans la cour intérieure du manoir.

---
+++ Project Unity/Assets/Scripts/Teleport_Same_Scene.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport_Same_Scene : MonoBehaviour {
+    public Transform cible;
+    private bool devant;
+    private Collider perso;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "GameController")
+        {
+            devant = true;
+            perso = col;
+        }
+        else
+        {
+            devant = false;
+        }
+    }
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("bonjour");
+            perso.transform.position = cible.position;
+        }
+
+    }
+}
